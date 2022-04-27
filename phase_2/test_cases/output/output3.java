d2de70568c821d9bec22654c967112fd0d1a4752
moramadan98 <47933260+moramadan98@users.noreply.github.com>
2022-04-27 22:08:08 +0200

    Run test cases and add outputs files

---
+++ phase_2/test_cases/output/output3.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+class DoWhileExample {
+    public static void main(String[] args) {
+ArrayList<Integer> visited = new ArrayList<Integer>(); // Create an ArrayList object
+
+if (!visited.contains(1)) {
+  
+                visited.add(1);
+            }        
+        int i=1;
+        do{
+if (!visited.contains(2)) {
+  
+                visited.add(2);
+            }        
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+try {
+            FileWriter writer = new FileWriter("output_txt/outputTXT#3.txt", false);
+            //BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            writer.write("");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+try{
+FileWriter writer = new FileWriter("output_txt/outputTXT#3.txt", true);
+
+for(int l=0 ; l< visited.size(); l++){
+System.out.print("block no: ");
+System.out.println(visited.get(l));
+            //BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            writer.write(visited.get(l).toString());
+ writer.write("  ");
+}            writer.close();
+}         catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
