1fdfa5e593fcc7d055313453b7d40bdf5e626662
marwa <47919176+marowaadel@users.noreply.github.com>
2022-05-05 22:08:07 +0200

    Add files via upload

---
+++ phase_2/test_cases/Output_Java/output9.java
@@ -0,0 +1,68 @@
+import java.io.FileWriter;
+import java.util.*;
+import java.io.IOException;
+class functioncall{
+    public static void main(String[]args){
+visited.add(0);
+
+fun();
+
+    
+try {
+            FileWriter writer = new FileWriter("output_txt/outputTXT#9.txt", false);
+            writer.write(" ");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+try{
+FileWriter writer = new FileWriter("output_txt/outputTXT#9.txt", true);
+
+Iterator<Integer> my_iterator = visited.iterator();
+            while (my_iterator.hasNext()){
+                writer.write(my_iterator.next().toString());
+                writer.write("  ");
+}            writer.close();
+}         catch (IOException e) {
+            e.printStackTrace();
+        }
+}
+    public static void fun()
+    {
+visited.add(1);
+
+        int weeks=5;
+        int days=7;
+
+        // outer loop prints weeks
+        for(int i=1;i<=weeks;++i){
+visited.add(2);
+
+
+            System.out.println("Week: "+i);
+
+            // inner loop prints days
+            for(int j=1;j<=days;++j){
+visited.add(3);
+
+
+                if(weeks==3)
+                {
+visited.add(4);
+
+
+                    System.out.println("  Day: "+j);System.out.println("  Day: "+j);
+                }
+                else {
+visited.add(5);
+
+
+                    System.out.println("  Day: " + j);
+                }
+            }
+        }
+    }
+
+public static SortedSet<Integer> visited = new TreeSet<Integer>();
+}
+
