1a4abc417bbe87d5d8f5fa408cbdb4355c7c4432
marwa <47919176+marowaadel@users.noreply.github.com>
2022-05-05 22:07:37 +0200

    Add files via upload

---
+++ input_testCases/input9.java
@@ -0,0 +1,32 @@
+class functioncall{
+    public static void main(String[]args){
+fun();
+
+    }
+    public static void fun()
+    {
+        int weeks=5;
+        int days=7;
+
+        // outer loop prints weeks
+        for(int i=1;i<=weeks;++i){
+
+            System.out.println("Week: "+i);
+
+            // inner loop prints days
+            for(int j=1;j<=days;++j){
+
+                if(weeks==3)
+                {
+
+                    System.out.println("  Day: "+j);System.out.println("  Day: "+j);
+                }
+                else {
+
+                    System.out.println("  Day: " + j);
+                }
+            }
+        }
+    }
+}
+
